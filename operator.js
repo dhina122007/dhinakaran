f559b95e8186116647f80cfcbb227de2d85fdfc8
dhina122007 <>
2026-06-12 10:59:45 +0530

    operators

---
+++ operator.js
@@ -0,0 +1,9 @@
+// arthimetic
+var a=10
+var b=20
+console.log(a+b)//addition
+console.log(a-b)//subraction
+console.log(a*b)//multipication
+console.log(a/b)//division
+console.log(a%b)//modulos
+console.log(a**b)//expo
--- variable.js
+++ variable.js
@@ -3,7 +3,7 @@ var name="vellar"
 console.log(name)
 
 // uing let
-let age='19'
+let age=19
 console.log(age)
 // uing const
 const country="india"
