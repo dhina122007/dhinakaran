b1f1f8f94c9759dcc6be9ea9ec07515ff945f8d9
dhina122007 <>
2026-06-12 11:40:05 +0530

    comparision

--- operator.js
+++ operator.js
@@ -16,3 +16,14 @@ console.log(a-=10)
 console.log(a*=10)
 console.log(a/=10)
 console.log(a**=10)
+ 
+ 
+console.log("comparision operator")
+// comparision
+var a=10
+var b=20
+console.log(a==b)
+console.log(a!=b)
+console.log(a<=b)
+console.log(a>=b)
+
